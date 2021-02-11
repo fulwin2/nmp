3503cde4831d9b4371710ee60b5684854c00c2c1
root <root@devops-ubuntu.local>
2021-02-11 14:10:07 +0200

    Directory configured

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,32 +1,57 @@
-SCANNER_HOME
+SCANNER_HOME="/tmp/sonar-scanner-4.5.0.2216-linux"
 pipeline {
-	agent {
-	     label 'java-docker-slave'	
-	}	
-     
-	stages {
-		
+    agent {
+         label 'java-docker-slave'	
+    }	
+
+   /* environment {
+        sonar.working.directory = '/tmp'
+        DB_ENGINE    = 'sqlite'
+    }
+   */
+    stages {
+	
      stage("build & SonarQube analysis") {
-          node {
-              withSonarQubeEnv('SonarCube') {
-                 sh 'mvn clean package sonar:sonar'
-				 sh '''$SCANNER_HOME/bin/sonar-scanner \
-                       -Dsonar.java.binaries=target/classes \
-					   -Dsonar.projectBaseDir=/tmp \
-					   -Dsonar.projectKey=/tmp \
-                       -Dsonar.sources=src/main/java'''
-              }
-          }
+	 steps {
+              println "$SCANNER_HOME"
+              withSonarQubeEnv("SonarCube") {
+		  println "${env.SONAR_HOST_URL}"
+                 //sh 'mvn clean package sonar:sonar'
+		  sh """ ${SCANNER_HOME}/bin/sonar-scanner \
+                         -Dsonar.java.binaries=target/classes \
+		         -Dsonar.projectBaseDir=/tmp \
+		         -Dsonar.projectKey=mvn-project \
+			 -Dsonar.working.directory=${WORKSPACE}/.scannerwork \
+			 -Dsonar.sources=src/main/java
+		     """
+              }		  
+	 }
       }
 
       stage("Quality Gate"){
-          timeout(time: 1, unit: 'HOURS') {
-              def qg = waitForQualityGate()
-              if (qg.status != 'OK') {
-                  error "Pipeline aborted due to quality gate failure: ${qg.status}"
-              }
-          }
-      }
-		
+	steps {
+	      withSonarQubeEnv("SonarCube") {
+	         withCredentials([string(credentialsId: 'jenkins1', variable: 'SONAR_TOKEN')]) {
+	            sh "printenv"
+	            timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: true
+		    }
+	        }
+	      }
+          
 	}
+      }
+	
+	stage("NMP") {
+	    steps {	
+		try {
+	          sh "mvn compile"
+	          sh "mvn package"
+	          println "SUCCESS: ${BUILD_NUMBER}"
+		} catch (Exception e){
+		  testPassed = false	
+		}	
+		
+		    
+            }
 }
