c79cd5d5bc8cbb7a4b4fb79f7aab1bcaf72d00f2
fulwin2 <36399195+fulwin2@users.noreply.github.com>
2023-12-26 13:10:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+
+  const args = process.argv.slice(2); // The first two elements are 'node' and the script name
+
+
+  if (args.length === 0) {
+    console.error('Error: Please provide a value as a command line argument.');
+    process.exit(1); // Exit with an error code
+  }
+
+  const inputValue = args[0]; // Assuming you want the first argument
+
+  console.log(`Input Value from Command Line: ${inputValue}`);
+  
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
-
