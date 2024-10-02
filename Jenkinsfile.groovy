0dd6202ef9c7e88689f639256cd53e550ae557bf
fulwin2 <36399195+fulwin2@users.noreply.github.com>
2024-10-02 16:32:53 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,7 +17,7 @@ pipeline {
       steps {
         git url: 'https://github.com/cyrille-leclerc/multi-module-maven-project'
         withMaven {
-          sh "mvn clean verify"
+          sh "mvn compile"
         } // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe reports and FindBugs reports
       }
     }
