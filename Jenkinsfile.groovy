2cafbd6cf3d47e916a40543240d3cd3fa32295a0
fulwin2 <36399195+fulwin2@users.noreply.github.com>
2024-10-02 16:29:47 +0200

    12

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,15 +12,14 @@ pipeline {
 }
 */
 //
-stages {
-
-
-  stage("NMP") {
-    steps {
+  stages {
+    stage("Build") {
+      steps {
+        git url: 'https://github.com/cyrille-leclerc/multi-module-maven-project'
         withMaven {
           sh "mvn clean verify"
-        } 
+        } // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe reports and FindBugs reports
+      }
     }
-
   }
 }
