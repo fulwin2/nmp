00e9de7f7b4b1cb036603ce134088cbc5f13b316
fulwin2 <36399195+fulwin2@users.noreply.github.com>
2024-10-10 11:30:32 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,14 @@
 //SCANNER_HOME="/tmp/sonar-scanner-4.5.0.2216-linux"
 SCANNER_HOME="/home/jenkins/workspace/mvn_build_and_test/sonar-scanner-4.5.0.2216-linux"
+@Library('select_tag') _
 //Comment
 pipeline {
   agent {
     label 'jenkins-slave'
   }
+parameters {
+        choice(name: 'TAG_VERSION', choices: getGitHubTags("https://github.com/fulwin2/nmp/tags"), description: 'Choose the Git tag version to build')
+}
 
   /* environment {
   sonar.working.directory = '/tmp'
