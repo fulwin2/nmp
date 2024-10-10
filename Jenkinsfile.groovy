fea9b2c9bef554a0efeabccc7ce5ac2e038d6093
fulwin2 <36399195+fulwin2@users.noreply.github.com>
2024-10-10 13:13:43 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,12 +2,16 @@
 SCANNER_HOME="/home/jenkins/workspace/mvn_build_and_test/sonar-scanner-4.5.0.2216-linux"
 @Library('selecttag@master') _
 //Comment
+def getGitHubTagsLocal() {
+  def tags = getGitHubTags.fetchGitTags("https://github.com/fulwin2/nmp")
+  return tags ?: ['latest']
+}
 pipeline {
   agent {
     label 'jenkins-slave'
   }
 parameters {
-        choice(name: 'TAG_VERSION', choices: getGitHubTagsLocal("https://github.com/fulwin2/nmp/tags"), description: 'Choose the Git tag version to build')
+        choice(name: 'TAG_VERSION', choices: getGitHubTagsLocal(), description: 'Choose the Git tag version to build')
 }
 
   /* environment {
@@ -42,6 +46,3 @@ stages {
 }
 }
 
-def getGitHubTagsLocal() {
-  def tags = getGitHubTags.fetchGitTags("https://github.com/fulwin2/nmp")
-}
